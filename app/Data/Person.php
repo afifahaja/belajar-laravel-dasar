67c958c66385e35b4529a485aae10ef7044db6e8
afifahaja <afifahnoor58@gmail.com>
2023-06-22 18:58:30 +0800

    updateServiceContainer

--- app/Data/Person.php
+++ app/Data/Person.php
@@ -5,7 +5,7 @@ namespace app\Data;
 class Person 
 {
     public function __construct(
-        public string $fisrtName,
+        public string $firstName,
         public string $lastName,   
     )
     {
